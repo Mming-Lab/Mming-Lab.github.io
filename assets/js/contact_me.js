4d8308364dd8aefa124065abe4199f84ce664650
MIYANAGA.Tomohisa <yutari.club@gmail.com>
2024-04-15 18:14:55 +0900

    httyty

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -5,23 +5,23 @@ $(function() {
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
-      // additional error messages or events
+      // 追加のエラーメッセージまたはイベント
     },
     submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
+      event.preventDefault(); // デフォルトの送信動作を防ぐ
+      // FORMから値を取得する
 	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
+      var firstName = name; //  成功/失敗メッセージ用
+      // 成功/失敗メッセージの名前に空白がないかチェックする
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
       $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this.prop("disabled", true); // メッセージの重複を防ぐため、AJAX呼び出しが完了するまで送信ボタンを無効にする。
       $.ajax({
         url: url,
         type: "POST",
@@ -40,7 +40,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>メッセージが送信されました。</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -52,7 +52,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text(firstName +"様、申し訳ありません、。メールサーバーが応答していないようです。後でもう一度お試しください！"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
@@ -60,7 +60,7 @@ $(function() {
 
         complete: function() {
           setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+            $this.prop("disabled", false); // AJAX呼び出しの完了時に送信ボタンを再度有効にする
           }, 1000);
         }
       });
@@ -76,7 +76,7 @@ $(function() {
   });
 });
 
-/*When clicking on Full hide fail/success boxes */
+/*失敗/成功ボックスを完全に隠すをクリックした場合 */
 $('#name').focus(function() {
   $('#success').html('');
 });
